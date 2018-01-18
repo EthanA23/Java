54b26b5c9200edfcad19dfaf962cd84927b258d6
EthanA23 <EthanA23@users.noreply.github.com>
2018-01-18 08:40:31 -0800

    some stuff
    forgot to submit

--- src/Mission.java
+++ src/Mission.java
@@ -1,4 +1,46 @@
+/*
+Author: Ethan Aghai
+Date: 10/16/17 
+Created mission
+*/
 
-public interface Mission {
+import java.util.ArrayList;
 
+public class Mission {
+	
+	private String objective;
+	private int difficulty;
+	private ArrayList<Enemy> list;
+
+	//loaded constructor
+	public Mission(int difficulty, int numOfenemies) {
+		this.difficulty = difficulty;
+		this.list = new ArrayList<Enemy>();
+	}
+						
+	//getters and setters
+	public String getObjective() {
+		return this.objective;
+	}
+	
+	public void setObjectives(String objective) {
+		this.objective = objective;
+	}
+	public int getDifficulty() {
+		return this.difficulty;
+	}
+	
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
+	}
+	
+	private ArrayList<Enemy> getList() {
+		return this.list;
+	}
+	
+	private void setList(ArrayList<Enemy> list) {
+		this.list = list;
+	}
+	
+	
 }
