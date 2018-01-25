5fc3c4ddc1f21f67a2a6a5889d44430c6ed4fc1c
EthanA23 <EthanA23@users.noreply.github.com>
2018-01-25 09:47:04 -0800

    Enemy.
    I finished workeing on my Enemy class.

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,64 @@
-
+/*
+Author: Ethan Aghai
+Date: 1/18/18
+Date: 1/25/18 
+Enemy for TextAdventure
+*/
 public class Enemy {
-
+	
+	//object properties
+	private int difficultyLevel;
+	private int health;
+	private boolean isAlive;
+	
+	//class constructor
+	public Enemy() {
+		this.health = 100;
+		this.isAlive = true;
+		this.difficultyLevel = 1; 
+				
+	}
+	
+	public Enemy(int health) {
+		this.health = health;
+		this.isAlive = true;
+		
+	}
+	
+	//getters and setters
+	public void setDifficultyLevel(int difficultyLevel) {
+		this.difficultyLevel = difficultyLevel;
+	}
+	
+	public int getDifficultyLevel() {
+		return this.difficultyLevel;
+	
+	}
+	
+	public void setHelth(int health) {
+		this.health = health;
+		
+	}
+	
+	public int getHealth() {
+		return this.health;
+		
+	}
+	
+	public void setIsAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+		
+	}
+	
+	public boolean getIsAlive() {
+		return this.isAlive;
+		
+	}
+	
+	//toString function
+	public String toString() {
+		return " health " + this.health + " isAlive " + this.isAlive + " 1: " + this.difficultyLevel;
+		
+	}
+	
 }
