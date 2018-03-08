7c9404f6ea8dab380d998b10fd3115e16be323f2
EthanA23 <EthanA23@users.noreply.github.com>
2018-03-08 09:45:23 -0800

    work
    did username

--- src/testinput.java
+++ src/testinput.java
@@ -11,13 +11,11 @@ public class testinput{
 		//initiate Scanner object to get user input
 		Scanner input = new Scanner(System.in);
 		String userName = "";
-		int letterCount = 0;
 		int numberCount = 0;
 		
 		//prompt the user for userName
 		System.out.println("Create a username.");
-		System.out.println("Your username must be at least 4 characters long \n" 
-							+ "with at least 3 letters and at least 1 number...");
+		System.out.println("Your username must be at least 4 characters long.");
 		
 		while(true){
 			//get the user response
@@ -26,39 +24,18 @@ public class testinput{
 			//give feedback on what user gave as input
 			System.out.println("You chose your username: " + userName);
 			
-			//check your input is at least 4 characters
-			if(userName.length() >= 4) {
-				System.out.println("Hi, " + userName);
-				
-			}
-			else {//userName is not at least 4 characters
-				//tell the user that there is an error with the given userName
-				System.out.println("Username needs to be at least 4 characters long.");
-			}
-			
-			//check each character in userName for a number
-			for(int i = 0; i < userName.length(); i++){
-				//check is the current character is a digit
-				if(Character.isDigit(userName.charAt(i))){
-					numberCount++; //nuberCount = numberCount + 1
-				}
-				
-				//check if the current character is a letter
-				if(Character.isDigit(userName.charAt(i))){
-					letterCount++;
-				}
-			}
-			
-			//check of there is at least 1 number and 3 letters in userName
-			if(numberCount > 0 && letterCount > 2){
-				System.out.println("Your username passes validation.");
+			//check if there is at least 1 number and 3 letters in userName
+			if(userName.length() > 3){
+				System.out.println("Your username passes validation. " + "Hi, " + userName);
 				break;
-
+				
 			}
-			else {
-				System.out.println("UserName needs at least 3 letters and at least 1 number.");
+				//userName is not at least 4 characters
+				//tell the user that there is an error with the given userName
+			else{ 
+				System.out.println("Username must be at least 4 characters long.");
 			}
-			
+					
 		}//end of while-loop
 		
 	}//end of main
